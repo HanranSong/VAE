30ba096c8b21fa68fc0653f9ad6060fa6c4365b0
Hanran Song <hanransong@gmail.com>
2026-03-02 18:00:24 -0500

    Add tqdm

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import argparse
+import os
+
 import torch
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
+from tqdm import tqdm
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch-size', type=int, default=128)
-parser.add_argument('--epochs', type=int, default=10)
+parser.add_argument('--epochs', type=int, default=100)
 parser.add_argument('--no-accel', action='store_true')
 parser.add_argument('--seed', type=int, default=42)
 parser.add_argument('--log-interval', type=int, default=10)
@@ -80,54 +83,64 @@ def loss_function(recon_x, x, mu, logvar):
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    return BCE + KLD
+    return BCE + KLD, BCE, KLD
 
 
 def train(epoch):
     model.train()
-    train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
+    train_loss, train_bce, train_kld = 0, 0, 0
+    for i, (data, _) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+        loss, bce, kld = loss_function(recon_batch, data, mu, logvar)
         loss.backward()
         train_loss += loss.item()
+        train_bce += bce.item()
+        train_kld += kld.item()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
+    dataset_size = len(train_loader.dataset)
+    return train_loss / dataset_size, train_bce / dataset_size, train_kld / dataset_size
 
 
 def test(epoch):
     model.eval()
-    test_loss = 0
+    test_loss, test_bce, test_kld = 0, 0, 0
     with torch.no_grad():
         for i, (data, _) in enumerate(test_loader):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
+            loss, bce, kld = loss_function(recon_batch, data, mu, logvar)
+            test_loss += loss.item()
+            test_bce += bce.item()
+            test_kld += kld.item()
+            if i == 0 and epoch % args.log_interval == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                                      recon_batch.view(-1, 1, 28, 28)[:n]])
                 save_image(comparison.cpu(),
                          'results/reconstruction_' + str(epoch) + '.png', nrow=n)
-
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+    dataset_size = len(test_loader.dataset)        
+    return test_loss / dataset_size, test_bce / dataset_size, test_kld / dataset_size
 
 if __name__ == "__main__":
-    for epoch in range(1, args.epochs + 1):
-        train(epoch)
-        test(epoch)
-        with torch.no_grad():
-            sample = torch.randn(64, 20).to(device)
-            sample = model.decode(sample).cpu()
-            save_image(sample.view(64, 1, 28, 28),
-                       'results/sample_' + str(epoch) + '.png')
+    os.makedirs('results', exist_ok=True)
+
+    pbar = tqdm(range(1, args.epochs + 1))
+    for epoch in pbar:
+        train_loss, train_bce, train_kld = train(epoch)
+        test_loss, test_bce, test_kld = test(epoch)
+
+        if epoch % args.log_interval == 0:
+            with torch.no_grad():
+                sample = torch.randn(64, 20).to(device)
+                sample = model.decode(sample).cpu()
+                save_image(sample.view(64, 1, 28, 28),
+                        'results/sample_' + str(epoch) + '.png')
+                
+        pbar.set_postfix({
+            "Train Loss": f"{train_loss:.2f}",
+            "BCE": f"{train_bce:.2f}",
+            "KLD": f"{train_kld:.2f}",
+            "Test Loss": f"{test_loss:.2f}"
+        })
