ff8b67b3a870f5ed9dba118e6235d7d6dd8ed63d
Hanran Song <hanransong@gmail.com>
2026-03-03 10:52:23 -0500

    Reorganize everything

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import os
 
 import torch
 import torch.utils.data
-from torch import nn, optim
-from torch.nn import functional as F
+from torch import optim
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 from tqdm import tqdm
+
+from models.vae import VAE
+from utils.losses import loss_function
 
 
 parser = argparse.ArgumentParser()
@@ -39,51 +41,8 @@ test_loader = torch.utils.data.DataLoader(
     datasets.FashionMNIST('../data', train=False, transform=transforms.ToTensor()),
     batch_size=args.batch_size, shuffle=False, **kwargs)
 
-
-class VAE(nn.Module):
-    def __init__(self):
-        super(VAE, self).__init__()
-
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
-
-    def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
-
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
-
-    def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
-
-
 model = VAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
-    return BCE + KLD, BCE, KLD
 
 
 def train(epoch):
@@ -136,7 +95,7 @@ if __name__ == "__main__":
                 sample = torch.randn(64, 20).to(device)
                 sample = model.decode(sample).cpu()
                 save_image(sample.view(64, 1, 28, 28),
-                        'results/sample_' + str(epoch) + '.png')
+                           'results/sample_' + str(epoch) + '.png')
                 
         pbar.set_postfix({
             "Train Loss": f"{train_loss:.2f}",
